2a76ecfb9b762227e9356044f3ab3ee7cdc97108
PichchakornK <s6404062663215@email.kmutnb.ac.th>
2025-04-02 14:16:53 +0700

    .

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; 
 // import { getAnalytics } from "firebase/analytics"; 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
