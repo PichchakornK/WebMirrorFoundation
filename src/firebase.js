0f9a4cc6a1a6b0bd57fed370388618b0fe2e5311
Jidapacha <s6404062663029@email.kmutnb.ac.th>
2025-04-02 14:03:03 +0700

    pp

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,13 @@
 import { initializeApp } from "firebase/app";
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; 
+// import { getAnalytics } from "firebase/analytics"; 
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC_QprSUY-8LCXPExNHXn4xV9PJouSmThc",
   authDomain: "donation-center-9576e.firebaseapp.com",
   projectId: "donation-center-9576e",
-  storageBucket: "donation-center-9576e.firebasestorage.app",
+  storageBucket: "donation-center-9576e.appspot.com", // ✅ แก้ไขให้ถูกต้อง (ลบ .app ออก)
   messagingSenderId: "350514743029",
   appId: "1:350514743029:web:beed9623127544a80167bc",
   measurementId: "G-XLXQJ9L0VF"
@@ -19,6 +15,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app); 
+
+const db = getFirestore(app);
 
 export { db };
