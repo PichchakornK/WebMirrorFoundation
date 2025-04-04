16c0f1923cb62ad528aa9d027459d91f8116e03d
Jidapacha <s6404062663029@email.kmutnb.ac.th>
2025-04-04 10:30:07 +0700

    pp

--- src/pages/ActivityCard.js
+++ src/pages/ActivityCard.js
@@ -8,12 +8,14 @@ const ActivityCard = ({ name, description }) => {
         <Card className="h-100">
             <Card.Body className="d-flex flex-column">
                 <Card.Title>{name}</Card.Title>
-                <Card.Text className={expanded ? "" : "text-truncate"}>
+                <Card.Text className={expanded ? "text-full" : "text-truncate"}>
                     {description}
                 </Card.Text>
-                <Button variant="primary" onClick={() => setExpanded(!expanded)}>
-                    {expanded ? "ย่อข้อความ" : "อ่านรายละเอียดเพิ่มเติม"}
-                </Button>
+                <div className="mt-auto">
+                    <Button variant="primary" onClick={() => setExpanded(!expanded)}>
+                        {expanded ? "ย่อข้อความ" : "อ่านรายละเอียดเพิ่มเติม"}
+                    </Button>
+                </div>
             </Card.Body>
         </Card>
     );
